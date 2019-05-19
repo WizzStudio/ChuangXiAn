b4a5d2632c6e90590e92d4294bd773ad958b3702
chapaofan <zzmaolin@gmail.com>
2019-05-19 14:48:17 +0800

    创建项目

---
+++ src/main/java/com/chuangxian/ChuangXiAnApplication.java
@@ -0,0 +1,13 @@
+package com.chuangxian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuangXiAnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuangXiAnApplication.class, args);
+    }
+
+}
