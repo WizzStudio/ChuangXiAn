77b4e38e7fa885dd66a11691ea45d6d60d2b298b
chapaofan <zzmaolin>
2019-07-04 11:02:45 +0800

    图片上传

---
+++ src/main/java/com/chuangxian/controller/PictureController.java
@@ -0,0 +1,48 @@
+package com.chuangxian.controller;
+
+import com.chuangxian.entity.Picture;
+import com.chuangxian.service.PictureService;
+import com.chuangxian.util.FileUtil;
+import com.chuangxian.util.ToolSupport.UniversalResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ：Maolin
+ * @className ：PictureController
+ * @date ：Created in 2019/7/4 10:40
+ * @description： Picture upload and show
+ * @version: 1.0
+ */
+@RestController
+@RequestMapping("/picture")
+@CrossOrigin(allowCredentials = "true")
+public class PictureController {
+    @Resource
+    private FileUtil fileUtil;
+
+    @Resource
+    private PictureService pictureService;
+
+    @PostMapping("/upload")
+    public UniversalResponseBody uploadFiles(@RequestParam("file") MultipartFile[] file, @RequestParam("id")int id){
+        if(fileUtil.uploadHelper(file,id)){
+            return new UniversalResponseBody(0,"success");
+        }else{
+            return new UniversalResponseBody(1,"failed");
+        }
+    }
+    @GetMapping("/show")
+    public UniversalResponseBody getPictureURL(@RequestParam("activityId")int activityId){
+        List<Picture> temp = pictureService.getPictureByActivityId(activityId);
+        List<String> res = new ArrayList<>();
+        for (Picture row: temp) {
+            res.add(row.getPictureUrl());
+        }
+        return new UniversalResponseBody<>(0,"success",res);
+    }
+}
+
---
+++ src/main/java/com/chuangxian/util/FileUtil.java
@@ -0,0 +1,93 @@
+package com.chuangxian.util;
+
+
+
+import com.chuangxian.entity.Picture;
+import com.chuangxian.service.PictureService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
+
+/**
+ * @author ：Maolin
+ * @className ：FileUtil
+ * @date ：Created in 2019/7/4 10:37
+ * @description：files upload
+ * @version: 1.0
+ * Here is the File upload process util.
+ * It needs to be called in the controller layer.
+ * file It is the file upload from front page.
+ * 本文件存在一些结构体系和设计模式上的问题，该文件暂时需要载入service来在控制器被调用
+ */
+@Service
+public class FileUtil {
+    @Resource
+    private PictureService pictureService;
+    /**
+     * 使用uploadHelper来将多文件拆解，在调用单个文件上传函数，加入事务管理
+     * @param temp 从前端传来的多文件List
+     * @return 返回是否全部上传成功
+     */
+    @Transactional
+    public boolean uploadHelper(MultipartFile[] temp, int activityId){
+        boolean flag = true;
+        for(int incr = 0;incr < temp.length;incr++){
+            try {
+                if (temp[incr].getOriginalFilename() == null || temp[incr].getOriginalFilename().equals("")) {
+                    continue;
+                }
+            }catch (NullPointerException e){
+                e.printStackTrace();
+            }
+            if(upload(temp[incr],activityId)){
+                flag = true;
+            }else {
+                flag = false;
+            }
+        }
+        return flag;
+    }
+
+    private boolean upload(MultipartFile file,int activityId){
+        try{
+            if(file.isEmpty()){
+                return false;
+            }
+            //重新构造文件名，防止文件名重复
+            String fileName = String.valueOf(new Date().getTime());
+            //Please replace the file path in you particular project.
+            //String path = "/root/uploadpicture/" + fileName + "_" + activityId;
+            String path = "F:/uploadpicture/" + fileName + "_" + activityId;
+            String fileType = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+            path = path + fileType;
+            Picture temp = new Picture();
+            String picUrl = "/image/photo/" + fileName + "_" + activityId + fileType;
+            temp.setPictureUrl(picUrl);
+            temp.setActivityId(activityId);
+            java.io.File ttep = new java.io.File(path);
+            if(!ttep.getParentFile().exists()){
+                ttep.getParentFile().mkdir();
+            }
+            try {
+                byte[] bytes = file.getBytes();
+                Path paths = Paths.get(path);
+                Files.write(paths, bytes);
+                //Call the service to insert file url into database
+                this.pictureService.addNewPicture(temp);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return true;
+    }
+}
+
